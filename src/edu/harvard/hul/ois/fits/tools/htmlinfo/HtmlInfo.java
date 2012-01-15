303553b541de5d8b6455969ad962f6ae7531e0d9
Stefan Schindler <schind85@gmail.com>
2012-01-15 19:08:13 +0100

    initial commit

---
+++ src/edu/harvard/hul/ois/fits/tools/htmlinfo/HtmlInfo.java
@@ -0,0 +1,146 @@
+package edu.harvard.hul.ois.fits.tools.htmlinfo;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
+
+
+
+import edu.harvard.hul.ois.fits.Fits;
+import edu.harvard.hul.ois.fits.exceptions.FitsToolException;
+import edu.harvard.hul.ois.fits.tools.ToolBase;
+import edu.harvard.hul.ois.fits.tools.ToolInfo;
+import edu.harvard.hul.ois.fits.tools.ToolOutput;
+
+public class HtmlInfo extends ToolBase
+{
+	private boolean enabled = true;
+	public final static String xslt = Fits.FITS_HOME + "xml/htmlinfo/htmlinfo_to_fits.xslt";
+	public final static boolean showParsingTime = false;
+	private HtmlParser parser;
+		
+	public static float avgParsingTime;
+	public static float avgXmlCreationTime; 
+	public static long objectCount;
+	
+	public static float avgParsingTimeValid;
+	public static float avgXmlCreationTimeValid; 
+	public static long objectCountValid;
+
+	public HtmlInfo() throws FitsToolException 
+	{
+		info = new ToolInfo("HtmlInfo", HtmlParser.PARSER_VERSION, null);		
+		//info = new ToolInfo("HtmlInfo", "0.1", null);
+	}
+
+	@Override
+	public ToolOutput extractInfo(File file) throws FitsToolException 
+	{								 
+		long startTimeParsing = 0;
+		long startTimeGenerating = 0;
+		float actualTimeParsing = 0.0f;
+		float actualTimeGenerating = 0.0f;
+		
+		if(showParsingTime)
+			startTimeParsing = System.nanoTime();
+		
+		parser = new HtmlParser(file.getAbsolutePath());
+		
+		if(showParsingTime)		
+		{			
+			long estimatedTimeParsing = System.nanoTime() - startTimeParsing;
+			// increase object count
+			objectCount++;
+			actualTimeParsing = (estimatedTimeParsing / 1000000f);
+			avgParsingTime = ((objectCount - 1) * avgParsingTime + actualTimeParsing) / objectCount;
+		}
+				
+//		System.out.println("Parsing time: " + (estimatedTime / 1000000f) + "ms");
+		if(showParsingTime)
+			startTimeGenerating = System.nanoTime();
+		
+      Document rawXml = parser.createXml();
+      
+      if(showParsingTime)
+      {
+	      long estimatedTimeGenerating = System.nanoTime() - startTimeGenerating;
+	      actualTimeGenerating = (estimatedTimeGenerating / 1000000f);
+			avgXmlCreationTime = ((objectCount - 1) * avgXmlCreationTime + actualTimeGenerating) / objectCount;
+      }
+      if(showParsingTime)
+      {
+			try {
+				List errors = XPath.selectNodes(rawXml, "//htmlInfo/error");
+				if(errors.size() == 0)
+				{
+					objectCountValid++;
+					avgParsingTimeValid = ((objectCount - 1) * avgParsingTimeValid + actualTimeParsing) / objectCountValid;
+					avgXmlCreationTimeValid = ((objectCount - 1) * avgXmlCreationTimeValid + actualTimeGenerating) / objectCountValid;
+				}
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+      }
+		
+		if(showParsingTime)
+		{
+			System.out.println("objects: " + objectCount);
+			System.out.println("avg parsing time: " + avgParsingTime + " ms");
+			System.out.println("avg xml creation time: " + avgXmlCreationTime + " ms");
+			System.out.println("valid objects: " + objectCountValid);
+			System.out.println("v.o. avg parsing time: " + avgParsingTimeValid + " ms");
+			System.out.println("v.o. avg xml creation time: " + avgXmlCreationTimeValid + " ms");
+		}
+		Document fitsXml = transform(xslt, rawXml);
+		return new ToolOutput(this, fitsXml, rawXml);
+	}
+	
+	private Document createXml() throws FitsToolException
+	{		
+      // xml root
+      Element root = new Element("htmlInfo");
+
+      // xml tags element
+      Element tags = new Element("tags");		
+		root.addContent(tags);
+						
+		Map<String, Long> tagMap = parser.getTags();
+		
+		for(String tagStr : tagMap.keySet())
+		{
+			Element tag = new Element("tag");
+			Element tagName = new Element("name");
+			Element tagCount = new Element("occurences");
+			
+			// set tag name and number of occurrences in file
+			tagName.setText(tagStr);
+			tagCount.setText(tagMap.get(tagStr).toString());
+			
+			tag.addContent(tagName);
+			tag.addContent(tagCount);
+			
+			tags.addContent(tag);
+		}
+		
+		return new Document(root);
+	}
+
+	@Override
+	public boolean isEnabled() 
+	{
+		return this.enabled;
+	}
+
+	@Override
+	public void setEnabled(boolean value) 
+	{
+		this.enabled = value; 
+	}
+}
---
+++ src/edu/harvard/hul/ois/fits/tools/htmlinfo/HtmlParser.java
@@ -0,0 +1,280 @@
+package edu.harvard.hul.ois.fits.tools.htmlinfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.nodes.RemarkNode;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.tags.FrameSetTag;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.tags.ObjectTag;
+import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+import org.jdom.Document;
+import org.jdom.Element;
+
+import edu.harvard.hul.ois.fits.Fits;
+import edu.harvard.hul.ois.fits.exceptions.FitsToolException;
+
+
+public class HtmlParser 
+{
+	//private static final Logger LOG = LoggerFactory.getLogger(HtmlParser.class);
+	public final static String xslt = Fits.FITS_HOME + "xml/htmlinfo/htmlinfo_to_fits.xslt";
+	
+	private Map<String, Long> tags = new HashMap<String, Long>();
+	private Map<String, Long> objects = new HashMap<String, Long>();
+	private long absoluteLinks = 0;
+	private long relativeLinks = 0;
+	private long frameSetCount = 0;
+	
+	// indicates, if a tag was found (--> html file)
+	private boolean foundTag = false;
+	
+	public Map<String, Long> getTags() {
+		return tags;
+	}
+	
+	public Map<String, Long> getObjects() {
+		return objects;
+	}
+
+	public static final String PARSER_VERSION = "0.3";
+	
+	public HtmlParser(String filename)
+	{
+		try {
+			Parser parser = new Parser(filename);
+	/*		NodeList list = parser.parse(null);
+			Node[] nodes = list.toNodeArray();
+			for(Node node : nodes)
+			{
+				//if(node instanceof TagNode)
+				//	System.out.println(((TagNode) node).getTagName());
+				System.out.println(node.getClass());
+			}			 */
+
+			// parse all nodes			
+			for (NodeIterator i = parser.elements (); i.hasMoreNodes (); )
+				if(processNode (i.nextNode ()) == false)
+					break;
+			
+	/*		System.out.println("Tag overview:");
+			 //Set<String> tags = tags.keySet();
+			 for(String tag : tags.keySet())
+			 {
+				 System.out.println(tag + " : " + tags.get(tag));
+			 }
+			 
+			 System.out.println("Object overview:");
+			 for(String tag : objects.keySet())
+			 {
+				 System.out.println(tag + " : " + objects.get(tag));
+			 }
+			 
+			 System.out.println("internal links: " + internalLinks);
+			 System.out.println("external links: " + externalLinks);*/
+			 			 
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+/*	public static void main(String[] args)
+	{
+		/*File[] files = new File("D:\\TU\\dipl\\files").listFiles();
+		for(int i = 0; i < files.length; i++)
+		{		
+			
+			HtmlParser parser = new HtmlParser(files[i].getAbsolutePath());
+			if(i%50 == 0)
+				//LOG.info("file " + i + " of total " + files.length + " examined");
+		}
+		
+		
+		try {
+			HtmlParser parser = new HtmlParser("D:\\TU\\dipl\\files\\temp");
+		} catch (FitsToolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		 
+	}		*/
+		
+	private boolean processNode (Node node)
+	{
+		if (node instanceof TextNode)
+		{
+			// first node no tag --> no html file --> return false
+			if(foundTag == false)
+				return false;
+			// TODO check ob schon Tags gefunden (nicht-html-files werden als textnodes interpretiert)
+			
+			// downcast to TextNode
+			// TextNode text = (TextNode)node;
+	      //System.out.println ("TEXT NODE: " + text.getText ());
+		}
+		else if (node instanceof RemarkNode)
+		{	      
+	      // do whatever processing you want with the comment
+		}
+		else if (node instanceof TagNode)
+		{			
+			foundTag = true;
+			
+	      // downcast to TagNode
+	      TagNode tag = (TagNode)node;
+	      
+	      // count tagnames
+	      incrementMapCount(tags, tag.getTagName(), 1);
+	      
+	      if(tag instanceof LinkTag)
+	      {
+	      	LinkTag lTag = (LinkTag)tag;
+	      		      	
+	      	if(checkLinkRef(lTag.extractLink()) == true)
+	      		absoluteLinks++;
+	      	else
+	      		relativeLinks++;
+	      }	      	
+	      	      
+	      if(tag instanceof ObjectTag)
+	      {
+	      	/*
+	      	 * The type attribute lets the author define the MIME type of the data used in the object—the file that’s specified 
+	      	 * in the data attribute. This is slightly different from the codetype attribute, which is used to specify 
+	      	 * the MIME type of the object itself. If the server sends data with the appropriate MIME type, this attribute may be omitted.
+	      	 */
+	      	
+	      	ObjectTag oTag = (ObjectTag)tag;
+	      	/*System.out.println("objectclassid:" + oTag.getObjectClassId());
+	      	System.out.println("codebase:" + oTag.getObjectCodeBase());
+	      	System.out.println("codetype:" + oTag.getObjectCodeType());
+	      	System.out.println("data:" + oTag.getObjectData());
+	      	System.out.println("standby:" + oTag.getObjectStandby());*/
+	      	if(oTag.getObjectType() == null)
+	      		incrementMapCount(objects, "undefined", 1);
+	      	else
+	      		incrementMapCount(objects, oTag.getObjectType(), 1);
+	      }
+	      
+	      if(tag instanceof FrameSetTag)
+	      {
+	      	//String[] ids = ((FrameSetTag) tag).getIds();
+	      	frameSetCount++;
+	      }
+	      
+	      // process recursively (nodes within nodes) via getChildren()
+	      NodeList nl = tag.getChildren();
+	      if (nl != null)
+	      {
+	      	try 
+	      	{
+					for (NodeIterator i = nl.elements (); i.hasMoreNodes() ; )						
+						processNode (i.nextNode ());
+				} catch (ParserException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	      }
+		}
+		return true;
+	 }
+	
+	protected void incrementMapCount(Map<String, Long> map, String key, long increment) {       
+       Long lw = (Long)map.get(key.toLowerCase());
+       if(lw == null) {
+      	 map.put(key.toLowerCase(), new Long(increment));
+       } else {
+      	 map.put(key.toLowerCase(), lw.longValue() + increment);
+       }
+   }
+	
+	/**
+	 * 
+	 * @param link
+	 * @return if the link is absolute (true) or relative (false)
+	 */
+	private boolean checkLinkRef(String link)
+	{
+		/*
+		 * relative links look like this:
+		 * file://localhost/aktuelles
+		 * 
+		 * absolute links look like this:
+		 * http://www.tuwien.ac.at/
+		 */
+		if (link.matches("http://.*"))
+			return true;
+		return false;
+	}
+	
+	public Document createXml() throws FitsToolException
+	{
+		// no xml file
+		if(foundTag == false)
+			return new Document(new Element("htmlInfo").addContent(new Element("error").setText("No XML File")));
+		// no html file		
+		if(!this.getTags().containsKey("html"))
+			return new Document(new Element("htmlInfo").addContent(new Element("error").setText("No HTML File")));
+      // xml root
+      Element root = new Element("htmlInfo");
+
+      // xml tags element
+      Element tags = new Element("tags");		
+		root.addContent(tags);
+						
+		Map<String, Long> tagMap = this.getTags();
+		
+		for(String tagStr : tagMap.keySet())
+		{
+			Element tag = new Element("tag");
+			Element tagName = new Element("name");
+			Element tagCount = new Element("occurences");
+			
+			// set tag name and number of occurrences in file
+			tagName.setText(tagStr);
+			tagCount.setText(tagMap.get(tagStr).toString());
+			
+			tag.addContent(tagName);
+			tag.addContent(tagCount);
+			
+			tags.addContent(tag);
+		}
+		
+		// xml objects element
+		Element objects = new Element("objects");
+		root.addContent(objects);
+		Map<String, Long> objectMap = this.getObjects();
+		
+		for(String objStr : objectMap.keySet())
+		{
+			Element obj = new Element("object");
+			Element tagType = new Element("type");
+			Element tagCount = new Element("occurences");
+			
+			// set tag name and number of occurrences in file
+			tagType.setText(objStr);
+			tagCount.setText(objectMap.get(objStr).toString());
+			
+			obj.addContent(tagType);
+			obj.addContent(tagCount);
+			
+			objects.addContent(obj);
+		}
+		
+		// link count
+		Element linkCount = new Element("linkCount");
+		root.addContent(linkCount);
+		linkCount.addContent(new Element("absolute").setText(""+this.absoluteLinks));
+		linkCount.addContent(new Element("relative").setText(""+this.relativeLinks));
+				
+		return new Document(root);
+	}
+}
